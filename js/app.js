b47332c48ff0df9bf890e051b286177eff0e6b19
Ordanis Sanchez <ordanisanchez@gmail.com>
2017-04-20 21:42:32 -0400

    Add sidepanel functionaly and create markers

--- js/app.js
+++ js/app.js
@@ -9,20 +9,73 @@
     {
       name: 'Zona Colonial',
       position: {
-        lat: 18.4743124,
-        lng: -69.8912537,
+        lat: 18.4722516,
+        lng: -69.8866591
       },
       info: ''
-    }
+    },
+    {
+      name: 'Centro Olimpico',
+      position: {
+        lat: 18.477685,
+        lng: -69.916742
+      },
+      info:''
+    },
+    {
+      name: 'Jardin Botanico',
+      position: {
+        lat: 18.4935752,
+        lng: -69.9535472
+      },
+      info:''
+    },
+    {
+      name: 'Faro a Colon',
+      position: {
+        lat: 18.4784251,
+        lng: -69.867889
+      },
+      info:''
+    },
   ];
 
-  const mapViewModel = {
-    activeNav: ko.observable(false),
-    activateSidePanel() {
-      this.activeNav(!this.activeNav());
-    },
-  };
+  class Location {
+    constructor(location) {
+      this.name = location.name;
+      this.location = location
+      this.marker = new google.maps.Marker({
+        position: location.position,
+        map: map,
+        title: location.name
+      })
+    }
+  }
 
+  const mapViewModel = () => {
+    // Observables
+    this.activeNav = ko.observable(false);
+
+    this.activateSidePanel = () => {
+      this.activeNav(!this.activeNav());
+    };
+    this.locations = ko.observableArray([]);
+
+    this.filter = ko.observable("");
+
+    this.filterLocations = ko.computed(() => {
+      const filter = this.filter().toLowerCase();
+
+      if (!filter) return this.locations();
+      return _.filter(this.locations(), item => {
+        return item.location.name.toLowerCase()
+                   .includes(filter);
+      })
+    });
+    // Insert default locations
+    model.forEach(location => this.locations.push(new Location(location)));
+  };
+  
   $('#map').css('height', window.innerHeight - 35);
   ko.applyBindings(mapViewModel);
 })();
